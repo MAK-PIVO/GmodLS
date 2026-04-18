57d86189deccb9c666c398bd88eb6a13aeae1162
MAK_PIVO <103646824+MAK-PIVO@users.noreply.github.com>
2026-04-18 21:01:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ let index = 0;
 
 function nextSlide() {
     slides[index].classList.remove("active");
-
     index = (index + 1) % slides.length;
-
     slides[index].classList.add("active");
 }
 
-// Change toutes les 5 secondes
 setInterval(nextSlide, 5000);
 
+// AUDIO CONTROL
+let music = document.getElementById("bgmusic");
+let volumeSlider = document.getElementById("volumeSlider");
 
-// GMod loading text
+// начальная громкость
+music.volume = volumeSlider.value;
+
+// изменение громкости
+volumeSlider.addEventListener("input", function () {
+    music.volume = this.value;
+});
+
+// фикс для автоплея (GMod иногда блочит)
+document.addEventListener("click", () => {
+    music.play().catch(() => {});
+}, { once: true });
+
+// GMod hooks
 function GameDetails(servername) {
-    document.querySelector("h1").innerText = servername;
+    document.querySelector("h1")?.innerText = servername;
 }
 
 function SetStatusChanged(status) {
-    document.getElementById("status").innerText = status;
+    document.getElementById("status")?.innerText = status;
 }
